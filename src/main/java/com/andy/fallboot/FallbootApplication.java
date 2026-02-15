bf857864c3b254f9aa51500ea44b34ac4c535c9c
Andy Chhuon <andychhuon@Andys-MacBook-Air.local>
2026-02-15 14:06:06 -0500

    Add basic spring app

---
+++ src/main/java/com/andy/fallboot/FallbootApplication.java
@@ -0,0 +1,13 @@
+package com.andy.fallboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FallbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FallbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andy/fallboot/FallbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.fallboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FallbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
